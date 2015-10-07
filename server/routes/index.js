261af0f00e432a3cf8160ee89e69458ee6fe2efd
Luis <delapenl@gmail.com>
2015-10-07 08:56:03 -0600

    started with face plus plus

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,23 +3,23 @@ var router = express.Router();
 var key = require('../../_config.js');
 var request = require('request');
 
-// router.get('/api', function(req, res, next){
-//   var imgUrl = 'http://www.online-image-editor.com//styles/2014/images/example_image.png';
-//   request({
-//     method: 'GET',
-//     url: 'http://gateway-a.watsonplatform.net/calls/url/URLGetRankedImageKeywords',
-//     params: {
-//       url: 'http://www.online-image-editor.com//styles/2014/images/example_image.png',
-//       apikey: 'dfc8ffa9897e45e8753a1e3c63b1ef1791208403'
-//     }
-//   }, function(err, response){
-//     if(err){
-//       res.json(err);
-//     } else {
-//       res.json(response);
-//     }
-//   });
-// })
+router.get('/api', function(req, res, next){
+  var imgUrl = 'http://www.online-image-editor.com//styles/2014/images/example_image.png';
+  request({
+    method: 'GET',
+    url: 'http://gateway-a.watsonplatform.net/calls/url/URLGetRankedImageKeywords',
+    params: {
+      url: 'http://www.online-image-editor.com//styles/2014/images/example_image.png',
+      apikey: 'dfc8ffa9897e45e8753a1e3c63b1ef1791208403'
+    }
+  }, function(err, response){
+    if(err){
+      res.json(err);
+    } else {
+      res.json(response);
+    }
+  });
+});
 
 router.post('/api/vision', function(req, res){
   console.log('made it to the GET');
@@ -54,6 +54,64 @@ router.post('/api/instagram', function(req, res) {
     }
   });
 });
+
+router.post('/api/facetags', function(req, res){
+  console.log('made it to the GET');
+  var imgUrl = req.body.imgUrl;
+  // var imgUrl = 'http://www.online-image-editor.com//styles/2014/images/example_image.png';
+  request({
+    method: 'GET',
+    url: 'http://gateway-a.watsonplatform.net/calls/url/URLGetRankedImageFaceTags?url='
+          +imgUrl+'&apikey='+key.alchemyKey+'&outputMode=json'
+  }, function(err, response){
+    if(err){
+      console.log('err', err);
+      res.json(err);
+    } else {
+      res.json(response);
+    }
+  });
+});
+
+router.post('/api/instagramfacetags', function(req, res) {
+  console.log('made it to the GET');
+  var keyword = req.body.keyword;
+  request({
+    method: 'GET',
+    url: "https://api.instagram.com/v1/tags/" + keyword + "/media/recent?client_id="+key.instagramKey
+  }, function(err, response){
+    if(err){
+      console.log('err', err);
+      res.json(err);
+    } else {
+      res.json(response);
+    }
+  });
+});
+
+router.post('/api/entities', function(req, res){
+  var imgUrl = req.body.imgUrl;
+  request({
+    method: 'GET',
+    url: 'http://gateway-a.watsonplatform.net/calls/url/URLGetRankedNamedEntities?url='
+          +imgUrl+'&apikey='+key.alchemyKey+'&outputMode=json'
+  }, function(err, response){
+    if(err){
+      console.log('err', err);
+      res.json(err);
+    } else {
+      res.json(response);
+    }
+  });
+});
+
+router.post('/api/facedetection', function(req, res) {
+  var imgUrl = req.body.imgUrl;
+  request({
+    method: 'GET',
+    url: 'https://faceplusplus-faceplusplus.p.mashape.com/detection/detect'
+  })
+})
 
 // http://gateway-a.watsonplatform.net/calls/url/URLGetRankedImageKeywords?url=http://www.online-image-editor.com//styles/2014/images/example_image.png&apikey=dfc8ffa9897e45e8753a1e3c63b1ef1791208403
 
