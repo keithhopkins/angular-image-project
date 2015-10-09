4b51ca6717906bbc38f013dca686662dd8e1d3b4
keithhopkins <knhopkins412@gmail.com>
2015-10-09 12:54:27 -0600

    starts angular auth

--- client/js/main.js
+++ client/js/main.js
@@ -5,4 +5,16 @@ angular.module('imageApp', ['ngRoute', 'homeDirective'])
         templateUrl: 'home/home.html',
         controller: 'HomeController'
       });
+      .when('/login', {
+        templateUrl: 'auth/partials/login.html',
+        controller: 'LoginController'
+      })
+      .when('/logout', {
+        controller: 'LogoutController'
+      })
+      .when('/register', {
+        templateUrl: 'auth/partials/register.html',
+        controller: 'RegisterController'
+      })
+      .otherwise({redirectTo: '/'});
   });
